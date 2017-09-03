5dd5fd861cae2973b3d3dc78b5a56e5b82b603ab
Jovica Aleksic <jovica.aleksic@loopmode.de>
2017-09-03 11:16:01 +0200

    init from https://github.com/loopmode/electron-react-boilerplate

---
+++ app/containers/App.js
@@ -0,0 +1,13 @@
+// @flow
+import React, { Component } from 'react';
+import type { Children } from 'react';
+
+export default class App extends Component {
+  props: {
+    children: Children
+  };
+
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
